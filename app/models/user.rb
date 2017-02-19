cc90d53378b39ca0742f2f52d3ac21a5a4059ed1
Amber <amber@amberwilkie.com>
2017-02-19 09:24:10 +0100

    Add language list

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   GENDERS = ['Female', 'Male', 'Other', 'Prefer Not to Say']
+  LANGUAGES = ['Arabic', 'Azerbaijani', 'Basque', 'Bengali', 'Berber', 'Bulgarian', 'Burmese', 'Catalan', 'Chinese Wu', 'Chinese Yue', 'Chinese/Other', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'Estonian', 'Farsi', 'Finnish', 'French', 'Gaelic', 'Galician', 'German', 'Greek', 'Gujarati', 'Hausa', 'Hindi/Urdu', 'Hungarian', 'Italian', 'Japanese', 'Javanese', 'Kannada', 'Korean', 'Kurdish', 'Latvian', 'Lithuanian', 'Malay', 'Malayalam', 'Maltese', 'Mandarin', 'Marathi', 'Oriya', 'Polish', 'Portuguese', 'Punjabi', 'Romanian', 'Russian', 'Slovak', 'Slovenian', 'Spanish', 'Swahili', 'Swedish', 'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukranian', 'Vietnamese']
 end
