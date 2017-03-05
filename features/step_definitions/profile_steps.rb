52146c11481bf32cf19d9d62c69f48deffdda1c3
Amber <amber@amberwilkie.com>
2017-03-05 14:30:58 +0100

    Update user new and edit forms to include username

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -1,9 +1,9 @@
 Given(/^I am on the profile page for "([^"]*)"$/) do |email|
   user = User.find_by(email: email)
-  visit home_path(user)
+  visit user_path(user)
 end
 
 Then(/^I should be on the profile page for "([^"]*)"$/) do |name|
   user = User.find_by(name: name)
-  expect(current_path).to eq home_path(user)
+  expect(current_path).to eq user_path(user)
 end
