4be52ee74ca03d2e20bc35311eb0d78d04c007cb
Amber <amber@amberwilkie.com>
2017-03-12 20:48:14 +0100

    Test for user geocoding for latitude

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,4 +88,12 @@ RSpec.describe User, type: :model do
       expect(bob.conversations.first).to eq conversation
     end
   end
+
+  describe 'Geocoding' do
+    let(:bob) { FactoryGirl.create(:user)}
+
+    it 'should geocode' do
+      expect(bob.latitude.round(2)).to eq 57.70
+    end
+  end
 end
